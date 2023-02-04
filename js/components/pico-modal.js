b4f2daabb4f3d2df9e9a513918331f2a77adf5d0
Enubia <44695100+Enubia@users.noreply.github.com>
2023-02-04 13:05:02 +0100

    prettier for clientside js files

--- js/components/pico-modal.js
+++ js/components/pico-modal.js
@@ -1,104 +1,107 @@
 /*
-* Modal
-* Taken from https://github.com/picocss/pico/blob/master/docs/js/modal.js
-* pico has no js included, this is their implementation of a modal js
-*/
+ * Modal
+ * Taken from https://github.com/picocss/pico/blob/master/docs/js/modal.js
+ * pico has no js included, this is their implementation of a modal js
+ */
 (function (namespace) {
-    namespace.Modal = class Modal {
-        constructor(config = {}) {
-            this.config = {
-                isOpenClass: 'modal-is-open',
-                openingClass: 'modal-is-opening',
-                closingClass: 'modal-is-closing',
-                animationDuration: 400, // ms
-                ...config
-            }
+	namespace.Modal = class Modal {
+		constructor(config = {}) {
+			this.config = {
+				isOpenClass: 'modal-is-open',
+				openingClass: 'modal-is-opening',
+				closingClass: 'modal-is-closing',
+				animationDuration: 400, // ms
+				...config,
+			};
 
-            this.isOpenClass = this.config.isOpenClass;
-            this.openingClass = this.config.openingClass;
-            this.closingClass = this.config.closingClass;
-            this.animationDuration = this.config.animationDuration;
-            this.visibleModal = null;
+			this.isOpenClass = this.config.isOpenClass;
+			this.openingClass = this.config.openingClass;
+			this.closingClass = this.config.closingClass;
+			this.animationDuration = this.config.animationDuration;
+			this.visibleModal = null;
 
-            // Close with a click outside
-            document.addEventListener('click', event => {
-                if (this.visibleModal !== null) {
-                    const modalContent = this.visibleModal.querySelector('article');
-                    const isClickInside = modalContent.contains(event.target);
-                    !isClickInside && this.closeModal(this.visibleModal);
-                }
-            });
+			// Close with a click outside
+			document.addEventListener('click', (event) => {
+				if (this.visibleModal !== null) {
+					const modalContent = this.visibleModal.querySelector('article');
+					const isClickInside = modalContent.contains(event.target);
+					!isClickInside && this.closeModal(this.visibleModal);
+				}
+			});
 
-            // Close with Esc key
-            document.addEventListener('keydown', event => {
-                if (event.key === 'Escape' && this.visibleModal !== null) {
-                    this.closeModal(this.visibleModal);
-                }
-            });
-        }
+			// Close with Esc key
+			document.addEventListener('keydown', (event) => {
+				if (event.key === 'Escape' && this.visibleModal !== null) {
+					this.closeModal(this.visibleModal);
+				}
+			});
+		}
 
+		toggleModal(event) {
+			event.preventDefault();
+			const modal = document.getElementById(event.currentTarget.getAttribute('data-target'));
 
-        toggleModal(event) {
-            event.preventDefault();
-            const modal = document.getElementById(event.currentTarget.getAttribute('data-target'));
+			if (typeof modal !== 'undefined' && modal !== null && this.isModalOpen(modal)) {
+				this.closeModal(modal);
+			} else {
+				this.openModal(modal);
+			}
+		}
 
-            if ((typeof (modal) !== 'undefined' && modal !== null) && this.isModalOpen(modal)) {
-                this.closeModal(modal);
-            } else {
-                this.openModal(modal);
-            }
-        }
+		isModalOpen(modal) {
+			return modal.hasAttribute('open') && modal.getAttribute('open') != 'false'
+				? true
+				: false;
+		}
 
-        isModalOpen(modal) {
-            return modal.hasAttribute('open') && modal.getAttribute('open') != 'false' ? true : false;
-        }
+		openModal(modal) {
+			if (this.isScrollbarVisible()) {
+				document.documentElement.style.setProperty(
+					'--scrollbar-width',
+					`${this.getScrollbarWidth()}px`,
+				);
+			}
+			document.documentElement.classList.add(this.isOpenClass, this.openingClass);
+			setTimeout(() => {
+				this.visibleModal = modal;
+				document.documentElement.classList.remove(this.openingClass);
+			}, this.animationDuration);
+			modal.setAttribute('open', true);
+		}
 
-        openModal(modal) {
-            if (this.isScrollbarVisible()) {
-                document.documentElement.style.setProperty('--scrollbar-width', `${this.getScrollbarWidth()}px`);
-            }
-            document.documentElement.classList.add(this.isOpenClass, this.openingClass);
-            setTimeout(() => {
-                this.visibleModal = modal;
-                document.documentElement.classList.remove(this.openingClass);
-            }, this.animationDuration);
-            modal.setAttribute('open', true);
-        }
+		closeModal(modal) {
+			this.visibleModal = null;
+			document.documentElement.classList.add(this.closingClass);
+			setTimeout(() => {
+				document.documentElement.classList.remove(this.closingClass, this.isOpenClass);
+				document.documentElement.style.removeProperty('--scrollbar-width');
+				modal.removeAttribute('open');
+			}, this.animationDuration);
+		}
 
-        closeModal(modal) {
-            this.visibleModal = null;
-            document.documentElement.classList.add(this.closingClass);
-            setTimeout(() => {
-                document.documentElement.classList.remove(this.closingClass, this.isOpenClass);
-                document.documentElement.style.removeProperty('--scrollbar-width');
-                modal.removeAttribute('open');
-            }, this.animationDuration);
-        }
+		getScrollbarWidth() {
+			// Creating invisible container
+			const outer = document.createElement('div');
+			outer.style.visibility = 'hidden';
+			outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+			outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+			document.body.appendChild(outer);
 
-        getScrollbarWidth() {
+			// Creating inner element and placing it in the container
+			const inner = document.createElement('div');
+			outer.appendChild(inner);
 
-            // Creating invisible container
-            const outer = document.createElement('div');
-            outer.style.visibility = 'hidden';
-            outer.style.overflow = 'scroll'; // forcing scrollbar to appear
-            outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
-            document.body.appendChild(outer);
+			// Calculating difference between container's full width and the child width
+			const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
 
-            // Creating inner element and placing it in the container
-            const inner = document.createElement('div');
-            outer.appendChild(inner);
+			// Removing temporary elements from the DOM
+			outer.parentNode.removeChild(outer);
 
-            // Calculating difference between container's full width and the child width
-            const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+			return scrollbarWidth;
+		}
 
-            // Removing temporary elements from the DOM
-            outer.parentNode.removeChild(outer);
-
-            return scrollbarWidth;
-        }
-
-        isScrollbarVisible() {
-            return document.body.scrollHeight > screen.height;
-        }
-    }
+		isScrollbarVisible() {
+			return document.body.scrollHeight > screen.height;
+		}
+	};
 })(extend('Pico'));
--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 function extend(ns_string) {
-    window.Pico = window.Pico || {};
+	window.Pico = window.Pico || {};
 
-    let parts = ns_string.split('.');
-    let parent = window.Pico;
+	let parts = ns_string.split('.');
+	let parent = window.Pico;
 
-    if (parts[0] === 'Pico') {
-        parts = parts.slice(1);
-    }
+	if (parts[0] === 'Pico') {
+		parts = parts.slice(1);
+	}
 
-    for (let i = 0; i < parts.length; i++) {
-        if (!parent[parts[i]]) {
-            parent[parts[i]] = {};
-        }
+	for (let i = 0; i < parts.length; i++) {
+		if (!parent[parts[i]]) {
+			parent[parts[i]] = {};
+		}
 
-        parent = parent[parts[i]];
-    }
+		parent = parent[parts[i]];
+	}
 
-    return parent;
+	return parent;
 }
 
 function ready(fn) {
-    if (document.readyState !== 'loading') {
-        fn();
-    } else {
-        document.addEventListener('DOMContentLoaded', fn);
-    }
+	if (document.readyState !== 'loading') {
+		fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
 }
