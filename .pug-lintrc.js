1d1aa85eaca17249c4c1a9b5f9d3fd6289c10446
Enubia <44695100+Enubia@users.noreply.github.com>
2023-02-11 18:16:15 +0100

    added pug-lint

---
+++ .pug-lintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "disallowClassAttributeWithStaticValue": true,
+    "disallowIdAttributeWithStaticValue": true,
+
+    "requireClassLiteralsBeforeAttributes": true,
+    "requireIdLiteralsBeforeAttributes": true,
+    "disallowClassLiteralsBeforeIdLiterals": true,
+
+    "disallowDuplicateAttributes": true,
+    "disallowMultipleLineBreaks": true,
+    "disallowTrailingSpaces": true,
+
+    "requireLowerCaseAttributes": true,
+    "requireLowerCaseTags": true,
+    "requireSpaceAfterCodeOperator": true,
+    "validateDivTags": true,
+    "validateIndentation": 4,
+    excludeFiles: ["node_modules/**"]
+};
